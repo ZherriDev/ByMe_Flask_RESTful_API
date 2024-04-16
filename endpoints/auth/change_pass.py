aca5775be5f474286c851de63252678e8a33f10b
Ravejaja <xukeita@proton.me>
2024-04-16 09:48:57 +0100

    End all sessions

--- endpoints/auth/change_pass.py
+++ endpoints/auth/change_pass.py
@@ -1,5 +1,6 @@
 from flask import jsonify, request, Blueprint, render_template
 from flask_mailman import EmailMultiAlternatives
+from flask_jwt_extended import jwt_required, verify_jwt_in_request
 from marshmallow import Schema, fields
 from sqlalchemy import text
 import bcrypt
@@ -8,6 +9,7 @@ from ..conn import Session
 change_password_bp = Blueprint('change_password', __name__)
 
 class ChangePasswordSchema(Schema):
+    doctor_id = fields.Str(required=True)
     old_password = fields.Str(required=True)
     new_password = fields.Str(required=True)
     confirm_password = fields.Str(required=True)
@@ -22,13 +24,26 @@ def change_password():
     if errors:
         return jsonify(errors), 400
     
+    jwt_data = verify_jwt_in_request()
+
+    jti = jwt_data['jti']
     session = Session()
 
     try:
         result = session.execute(
-            text(''),
-            {'email': data['email']}
-        )
+            text('UPDATE doctors SET password = :password WHERE doctor_id = :doctor_id'),
+            {
+                'doctor_id': data['doctor_id'],
+            },
+        ).fetchone()
+
+        if result:
+            session.execute(text('UPDATE sessions SET in_blacklist = 1 WHERE doctor_id = :doctor_id'),
+                {
+                    'doctor_id': data['doctor_id'],
+                },
+            )
+
     except Exception as e:
         return jsonify({'error': str(e)}), 400
     finally:
