009a0351312854bcba7b8b3da0a21c93da1caf66
Ravejaja <xukeita@proton.me>
2024-04-17 16:35:46 +0100

    Shutdown endpoint

--- endpoints/admin/shutdown.py
+++ endpoints/admin/shutdown.py
@@ -5,4 +5,12 @@ shutdown_bp = Blueprint('shutdown', __name__)
 
 @shutdown_bp.route('/shutdown', methods=['GET'])
 def shutdown():
-    
+    if request.remote_addr == '127.0.0.1':
+        shutdown_func = request.environ.get('werkzeug.server.shutdown')
+        if shutdown_func is None:
+            return jsonify({'error': 'no shutdown function'}), 500
+        
+        shutdown_func()
+        return jsonify({'success': True, 'msg': 'Shutting down...'}), 200
+    else:
+        return jsonify({'error': 'Access Denied'}), 403
