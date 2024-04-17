00f5ddd3f97062c74158b606dc1ef215dee9ab0a
Ravejaja <xukeita@proton.me>
2024-04-17 16:20:05 +0100

    Shutdown server

--- endpoints/conn.py
+++ endpoints/conn.py
@@ -2,25 +2,32 @@ from sqlalchemy import create_engine, Column, Integer, String, BigInteger, DateT
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.exc import SQLAlchemyError
+from flask import request
+import sys
 
-user_db = 'BymeDB_bricklift'
-password_db = 'e40f766b1e3ab2a917d8328d912757f02b0a8d82'
-host_db = '8r0.h.filess.io'
-port_db = 3307
-database_db = 'BymeDB_bricklift'
+try:
+    user_db = 'BymeDB_bricklift'
+    password_db = 'e40f766b1e3ab2a917d8328d912757f02b0a8d82'
+    host_db = '8r0.h.filess.io'
+    port_db = 3307
+    database_db = 'BymeDB_bricklift'
 
-engine = create_engine(f'mysql+pymysql://{user_db}:{password_db}@{host_db}:{port_db}/{database_db}')
-Session = sessionmaker(bind=engine)
+    engine = create_engine(f'mysql+pymysql://{user_db}:{password_db}@{host_db}:{port_db}/{database_db}')
+    Session = sessionmaker(bind=engine)
 
-Base = declarative_base()
+    Base = declarative_base()
 
-class Log(Base):
-    __tablename__ = "logs"
+    class Log(Base):
+        __tablename__ = "logs"
 
-    log_id = Column(BigInteger, primary_key=True, autoincrement=True)
-    level = Column(String, nullable=True)
-    date_time = Column(DateTime, nullable=True)
-    msg = Column(String, nullable=True)
-    endpoint = Column(String, nullable=True)
-    method = Column(String, nullable=True)
-    status_code = Column(Integer, nullable=True)
+        log_id = Column(BigInteger, primary_key=True, autoincrement=True)
+        level = Column(String, nullable=True)
+        date_time = Column(DateTime, nullable=True)
+        msg = Column(String, nullable=True)
+        endpoint = Column(String, nullable=True)
+        method = Column(String, nullable=True)
+        status_code = Column(Integer, nullable=True)
+
+except SQLAlchemyError as e:
+    print(e)
+
