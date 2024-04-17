e8c9561971ad4fc2ca62133009d917988537eb2b
ZherriDev <zherri.dev@gmail.com>
2024-04-17 17:02:48 +0100

    shuting down

--- endpoints/admin/shutdown.py
+++ endpoints/admin/shutdown.py
@@ -1,15 +1,14 @@
 from flask import jsonify, request,  Blueprint
 from ..conn import Session
-import subprocess
+import signal
+import os
 
 shutdown_bp = Blueprint('shutdown', __name__)
 
 @shutdown_bp.route('/shutdown', methods=['GET'])
 def shutdown():
     if request.remote_addr == '127.0.0.1':
-        
-        cmd = "pkill -f 'gunicorn'"
-        subprocess.Popen(cmd, shell=True)
+        os.kill(os.getpid(), signal.SIGINT)
         return jsonify({'success': True, 'msg': 'Shutting down...'}), 200
     else:
         return jsonify({'error': 'Access Denied'}), 403
