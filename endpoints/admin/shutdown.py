087c9e4004b2001f098b260184b2431d1ff1c5b8
Ravejaja <xukeita@proton.me>
2024-04-17 16:48:47 +0100

    Shutdown Endpoint

--- endpoints/admin/shutdown.py
+++ endpoints/admin/shutdown.py
@@ -1,16 +1,15 @@
-from flask import jsonify, request, Blueprint
+from flask import jsonify, request,  Blueprint
 from ..conn import Session
+import subprocess
 
 shutdown_bp = Blueprint('shutdown', __name__)
 
 @shutdown_bp.route('/shutdown', methods=['GET'])
 def shutdown():
     if request.remote_addr == '127.0.0.1':
-        shutdown_func = request.environ.get('werkzeug.server.shutdown')
-        if shutdown_func is None:
-            return jsonify({'error': 'no shutdown function'}), 500
         
-        shutdown_func()
+        cmd = "pkill -f 'gunicorn'"
+        subprocess.Popen(cmd, shell=True)
         return jsonify({'success': True, 'msg': 'Shutting down...'}), 200
     else:
         return jsonify({'error': 'Access Denied'}), 403
