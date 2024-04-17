fdb60af4dd1552f26b9b5c2da88d553c32109fdb
ZherriDev <zherri.dev@gmail.com>
2024-04-17 11:58:40 +0100

    making a logger

--- endpoints/conn.py
+++ endpoints/conn.py
@@ -1,6 +1,6 @@
-from sqlalchemy import createengine, Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarativebase
+from sqlalchemy.ext.declarative import declarative_base
 
 user_db = 'BymeDB_bricklift'
 password_db = 'e40f766b1e3ab2a917d8328d912757f02b0a8d82'
