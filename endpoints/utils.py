aae1357fca0caed2923743e036f7115756c1683f
ZherriDev <zherri.dev@gmail.com>
2024-04-19 12:51:50 +0100

    fixing limiter default limit

--- endpoints/utils.py
+++ endpoints/utils.py
@@ -4,10 +4,9 @@ from flask_limiter.util import get_remote_address
 limiter = Limiter(
     get_remote_address,
     app=None,
-    default_limits=["none per minute"],
+    default_limits=["100 per minute"],
     storage_uri="memory://"
 )
 
 def set_app(app):
   limiter.init_app(app)  
-
